57efaea61e05cbde3ab99121341c8d16a227f592
Dan Fein <dan.fein@weather.com>
2017-03-22 15:36:37 -0700

    Function Junction
    Moved the loop code into functional modules

--- SparkWeather.ino
+++ SparkWeather.ino
@@ -24,24 +24,19 @@ This code assumes your wifi credentials are already saved in the CC3000's non-vo
 // This #include statement was automatically added by the Spark IDE.
 #include "idDHT22/idDHT22.h"
 
+// Data buckets
+    float tempf = 999;
+    float humidity = 999;
+    float dewptf = 999;    
+    
 // declaration for DHT11 handler
-int idDHT22pin = D4; //Digital pin for comunications
-void dht22_wrapper(); // must be declared before the lib initialization
+	int idDHT22pin = D4; //Digital pin for comunications
+	void dht22_wrapper(); // must be declared before the lib initialization
 
 // DHT instantiate
-idDHT22 DHT22(idDHT22pin, dht22_wrapper);
-
-//Timekeeping
-    int sec = 0;
-    int min = 0;
-    int hr = 0;
-    int day = 0;
-    int month = 0;
-    int year = 0;
+	idDHT22 DHT22(idDHT22pin, dht22_wrapper);
 
 //Wunderground
-    //IPAddress SERVER(38,102,137,157);  // numeric IP for wunderground.com
-    //char SERVER[] = "rtupdate.wunderground.com"; // Realtime update server
     char SERVER [] = "weatherstation.wunderground.com"; //standard server
     char WEBPAGE [] = "GET /weatherstation/updateweatherstation.php?";
 
@@ -52,19 +47,12 @@ idDHT22 DHT22(idDHT22pin, dht22_wrapper);
 //Conection nonsense
     TCPClient client;
 
+const unsigned long IDLE_TIMEOUT_MS = 700; // Time to wait for the WunderGround server to respond
 
+//-----------------------Run once---------------------
 void setup()
 {
 	Serial.begin(9600);
-	/*
-	while(!Serial.available()) {
-	    Serial.println("hit a key");
-	    delay(1000);
-	}*/
-	
-	Serial.println("idDHT22 Example program");
-	Serial.print("LIB version: ");
-	Serial.println(idDHT22LIB_VERSION);
 	Serial.println("---------------");
 }
 // This wrapper is in charge of calling
@@ -73,23 +61,34 @@ void dht22_wrapper() {
 	DHT22.isrCallback();
 }
 
-//plug the time into the time vars - The spark has a built-in library for time
-void stamp(){
-    sec = Time.second();
-    min = Time.minute();
-    hr = Time.hour();
-    day = Time.day();
-    month = Time.month();
-    year = Time.year(); 
+//-----------------------Run forever---------------------
+void loop(){
+    
+//print some tests
+	
+	grabData();		// Grab some data
+  	
+	serial();		// Print data to serial
+  	
+	sendToWU();		// Send it away  
+
+	listen();		// listens for server response confirming receipt
+	  
+	//Spark.sleep(SLEEP_MODE_DEEP,300); // Power down for a bit to save power
+}
+//-----------------------End of Program---------------------
+
+
+//-----------------Functions--------------------
+
+void grabData(){
+   	DHT22.acquire();
+    float tempf = (DHT22.getFahrenheit(), 2);
+    float humidity = (DHT22.getHumidity(), 2);
+    float dewptf = (DHT22.getDewPoint());    
 }
 
 void serial(){
-
-	Serial.print("\nRetrieving information from sensor: ");
-	Serial.print("Read sensor: ");
-	//delay(100);
-	DHT22.acquire();
-	
 	Serial.print("Humidity (%): ");
 	Serial.println(DHT22.getHumidity(), 2);
 
@@ -107,90 +106,35 @@ void serial(){
 
 	Serial.print("Dew Point Slow (oC): ");
 	Serial.println(DHT22.getDewPointSlow());
-  
-    
-    Serial.print("&dateutc=");
-    Serial.print(year);
-    Serial.print("-");
-    Serial.print(month);
-    Serial.print("-");
-    Serial.print(day);
-    Serial.print("+");
-    Serial.print(hr);
-    Serial.print("%3A");        // %3A is a colon (,)
-    Serial.print(min);
-    Serial.print("%3A");
-    Serial.print(sec);
- 
 }
 
-void loop(){
-    
-//get the time    
-    stamp(); 
-//print some tests
-    serial(); 
-//hold up    
-    delay(200); 
-//grab some data	
-    float tempf = (DHT22.getFahrenheit(), 2);
-    float humidity = (DHT22.getHumidity(), 2);
-    float dewptf = (DHT22.getDewPoint());    
-//Send it away    
-     SPARK_WLAN_Loop();
-     Serial.println("connecting...");
+void sendToWU(){
 
-	if (client.connect(SERVER, 80)) { 
-        Serial.println("Connected");
-        client.print(WEBPAGE);
-        client.print("ID=");
-        client.print(ID);
-        client.print("&PASSWORD=");
-        client.print(PASSWORD);
-        client.print("&dateutc="); 	//if sending in real time and you can send as 'dateutc=now' instead of timestamp
-        client.print(year);
-        client.print("-");
-        client.print(month);
-        client.print("-");
-        client.print(day);
-        client.print("+");
-        client.print(hr);	
-        client.print("%3A");        	// %3A is a colon (,)
-        client.print(min);
-        client.print("%3A");
-        client.print(sec);
-        //client.print("&winddir=");
-        //client.print(winddir);
-        //client.print("&windspeedmph=");
-        //client.print(windspeedmph);
-        //client.print("&windgustmph=");
-        //client.print(windgustmph);
-        client.print("&tempf=");
-        client.print(tempf);
-        //client.print("&rainin=");
-        //client.print(rainin / 2.0 / 100.0);
-        //client.print("&baromin=");
-        //client.print(baromin);
-        client.print("&dewptf=");
-        client.print(dewptf);
-        client.print("&humidity=");
-        client.print(humidity);
-        client.print("&action=updateraw");//Standard update
-        //client.print("&softwaretype=Spark%Core%20version1&action=updateraw&realtime=1&rtfreq=2.5");//Rapid Fire
-  		client.print(" HTTP/1.0\r\n");
-  		client.print("Accept: text/html\r\n");
-  		client.print("Host: ");
-  		client.print(SERVER);
-  		client.print("\r\n\r\n");
-		Serial.println("Upload complete");
+  Serial.print("connecting... ");
 
-	}
-	else {
-          Serial.println(F("Connection failed"));
-	  }
-
-    //hold up    
-        delay(5000); 
-    //Wait a minute   
-        Spark.sleep(SLEEP_MODE_DEEP,300); // Power down for a bit to save power
+  if (client.connect(SERVER, 80)) {
+  Serial.println("connected");
+  client.printf("%sID=%s&PASSWORD=%s&dateutc=now&tempf=%f&dewptf=%f&humidity=%f&baromin=%f&action=updateraw&softwaretype=ParticlePhoton&realtime=1&rtfreq=30 HTTP/1.0\r\n", WEBPAGE, ID, PASSWORD, tempF, dewptF, humidity, inches);
+  Serial.print("sending... ");
+  Serial.printf("%s&ID=%s&PASSWORD=%s&dateutc=now&tempf=%f&dewptf=%f&humidity=%f&baromin=%f&action=updateraw&softwaretype=ParticlePhoton&realtime=1&rtfreq=30 HTTP/1.0\r\n", WEBPAGE, ID, PASSWORD, tempF, dewptF, humidity, inches);
+  client.print("Accept: text/html\r\n"); //Tell the server you want a response
+  client.print("\r\n\r\n"); //end GET
+  Serial.println("upload complete");
+  }else{
+    Serial.println(F("connection failed"));
+  return;
+  }
+  
+void listen()
+{
+  Serial.println("Server Response...");
+  unsigned long lastRead = millis();
+  while (client.connected() && (millis() - lastRead < IDLE_TIMEOUT_MS))
+    {
+    if (client.available()) {
+      char c = client.read();
+      Serial.print(c);
+      }
+    }
+    Serial.println();
 }
